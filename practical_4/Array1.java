1301411bbaa0f2abed82a3b3c38b62328993e470
“student-om” <2022bit046@sggs.ac.in>
2024-01-28 11:18:15 +0530

    printing array elements

---
+++ practical_4/Array1.java
@@ -0,0 +1,12 @@
+class Array1{
+	static void arrPrint(int[] p){
+		for(int i=0;i<p.length;i++){
+		System.out.println(p[i]);
+		}
+	}
+	
+	public static void main(String[] args){
+	int[] q = {1,2,3,4,5,6};
+	arrPrint(q);
+	}
+}
