1ab9ca0623b218998edf1cb6acc6cb1f5ce96fc0
student-om <2022bit046@sggs.ac.in>
2024-04-07 22:49:16 +0530

    Day2

--- Daily_questions/Day1.java
+++ Daily_questions/Day1.java
@@ -10,11 +10,11 @@ class Day1{
 	}
 	
 	//catch(Exception e) ---> error will come(catch should be written at last)
-	catch(ArrayIndexOutOfBoundsException e){   // this is a run time exception
+	catch(ArrayIndexOutOfBoundsException e){   // this is a run time exception ifemen you remove this catch stil code compiles ans the statement gets printed(Unchecked exception)
 	System.out.println("wrong index");
 	}
 	//2 ArrayIndexOutOfBoundsException ARE also not allowed
-	catch(InterruptedException e){  //this is a compile time exception
+	catch(InterruptedException e){  //this is a compile time exception i.e if you remove this catch exception comes at compile-time
 	System.out.println("InterruptedException found");
 	}
 	catch(Exception e) {
